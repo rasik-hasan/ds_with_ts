eda20575b0dd9e220c583ee87ff5c4e885ddb984
Rasik Hasan <rasik.hasan14@gmail.com>
2022-03-03 03:38:02 +0600

    fib memoized

--- src/ProblemSolving/LC-509_fibonacci.ts
+++ src/ProblemSolving/LC-509_fibonacci.ts
@@ -1,5 +1,5 @@
 export const testFibonacci = () => {
-  const input = 4;
+  const input = 100;
 
   const fib = (n: number): number => {
     if (n === 1) {
@@ -11,5 +11,23 @@ export const testFibonacci = () => {
     return fib(n - 2) + fib(n - 1);
   };
 
-  console.log(fib(input));
+  const memoizeMap = new Map<number, number>();
+
+  const fibMemoize = (n: number): number => {
+    if (n === 1) {
+      return 1;
+    } else if (n === 0) {
+      return 0;
+    }
+
+    if (memoizeMap.has(n)) {
+      return memoizeMap.get(n)!;
+    } else {
+      let value = fibMemoize(n - 2) + fibMemoize(n - 1);
+      memoizeMap.set(n, value);
+      return value;
+    }
+  };
+
+  console.log(fibMemoize(input));
 };
