49e2d3d638b71cabf749cdb508aeede03f308e99
Rasik Hasan <rasik.hasan14@gmail.com>
2022-04-16 12:13:02 +0600

    time to debug

--- src/ProblemSolving/LC-200_number_of_islands.ts
+++ src/ProblemSolving/LC-200_number_of_islands.ts
@@ -46,47 +46,70 @@ export const testNumberOfIslands = () => {
   ];
 
   function numIslands(grid: string[][]): number {
+    interface tuples {
+      i: number;
+      j: number;
+    }
+
+    const rootValues = new Map<tuples, tuples>();
+
+    const reconcile = (oldValue: tuples, newValue: tuples) => {
+      rootValues.forEach((value, key) => {
+        if (value.i === oldValue.i && value.j === oldValue.j) {
+          rootValues.set(key, newValue);
+        }
+      });
+    };
+
     for (let i = 0; i < grid.length; i++) {
       for (let j = 0; j < grid[i].length; j++) {
         if (grid[i][j] === "1") {
-          const up = i > 0 ? grid[i - 1][j] : null;
-          const left = j > 0 ? grid[i][j - 1] : null;
+          const up = rootValues.has({ i: i - 1, j })
+            ? rootValues.get({ i: i - 1, j })
+            : null;
+          const left = rootValues.has({ i, j: j - 1 })
+            ? rootValues.get({ i, j: j - 1 })
+            : null;
 
-          //console.log(up, left);
+          const current: tuples = { i, j };
 
-          if (up === "2" || left === "2") {
-            grid[i][j] = "2";
-
-            if (up === "1") {
-              grid[i - 1][j] = "2";
+          //both left and up present
+          if (left && up) {
+            if (left.i === up.i && left.j === up.j) {
+              rootValues.set(current, { i: up.i, j: up.j });
+            } else {
+              // for now choose one and reconcile another
+              rootValues.set(current, up);
+              reconcile(left, up);
             }
-
-            if (left === "1") {
-              grid[i][j - 1] = "2";
+          } else if (left || up) {
+            //if either left or up is present
+            if (up) {
+              rootValues.set(current, up);
+            } else {
+              rootValues.set(current, left!);
             }
-          } else if (up === "1" || left === "1") {
-            grid[i][j] = "2";
+          } else {
+            //if neither is present
+            rootValues.set(current, current);
           }
         }
       }
     }
 
-    let numberOfIslands = 0;
+    console.log(rootValues);
 
-    for (let i = 0; i < grid.length; i++) {
-      for (let j = 0; j < grid[i].length; j++) {
-        if (grid[i][j] === "1") {
-          numberOfIslands++;
-        }
-      }
+    const values = Array.from(rootValues.values());
+
+    function onlyUnique(value: any, index: any, self: any) {
+      return self.indexOf(value) === index;
     }
+    const unique = values.filter(onlyUnique).length;
 
-    console.log(grid);
-
-    return numberOfIslands;
+    return unique;
   }
 
   //console.log(grid1);
-  console.log(numIslands(grid7));
+  console.log(numIslands(grid6));
   //console.log(grid1);
 };
