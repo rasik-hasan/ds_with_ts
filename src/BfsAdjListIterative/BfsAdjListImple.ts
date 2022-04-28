4162b623e5cdc32b3d946bc2264b8f363df4fc50
Rasik Hasan <rasik.hasan14@gmail.com>
2022-04-28 14:15:56 +0600

    new graph declaration seems alright

--- src/BfsAdjListIterative/BfsAdjListImple.ts
+++ src/BfsAdjListIterative/BfsAdjListImple.ts
@@ -1,3 +1,12 @@
+import {
+  addUnweightedUndirectedEdge,
+  createEmptyGraph,
+} from "./BfsAdjListIterative";
+
 export const testBfsAdjList = () => {
   console.log("test bfs");
+
+  const graph = createEmptyGraph(13);
+  addUnweightedUndirectedEdge(graph, 0, 7);
+  console.log(graph);
 };
---
+++ src/BfsAdjListIterative/BfsAdjListIterative.ts
@@ -0,0 +1,47 @@
+export class Edge {
+  private from: number;
+  private to: number;
+  private cost: number;
+
+  constructor($from: number, $to: number, $cost: number) {
+    this.from = $from;
+    this.to = $to;
+    this.cost = $cost;
+  }
+}
+
+export const createEmptyGraph = (n: number): Edge[][] => {
+  const graph: Edge[][] = new Array(n);
+
+  for (let i = 0; i < n; i++) {
+    graph[i] = [];
+  }
+  return graph;
+};
+
+export const addDirectedEdge = (
+  graph: Edge[][],
+  from: number,
+  to: number,
+  cost: number
+) => {
+  graph[from].push(new Edge(from, to, cost));
+};
+
+export const addUndirectedEdge = (
+  graph: Edge[][],
+  from: number,
+  to: number,
+  cost: number
+) => {
+  addDirectedEdge(graph, from, to, cost);
+  addDirectedEdge(graph, to, from, cost);
+};
+
+export const addUnweightedUndirectedEdge = (
+  graph: Edge[][],
+  from: number,
+  to: number
+) => {
+  addUndirectedEdge(graph, from, to, 1);
+};
