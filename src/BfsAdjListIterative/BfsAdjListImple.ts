50cdd1c90172525389c8bf14eef42fa7ee61dc95
Rasik Hasan <rasik.hasan14@gmail.com>
2022-04-28 15:33:38 +0600

    ready to work

--- src/BfsAdjListIterative/BfsAdjListImple.ts
+++ src/BfsAdjListIterative/BfsAdjListImple.ts
@@ -8,5 +8,19 @@ export const testBfsAdjList = () => {
 
   const graph = createEmptyGraph(13);
   addUnweightedUndirectedEdge(graph, 0, 7);
+  addUnweightedUndirectedEdge(graph, 0, 9);
+  addUnweightedUndirectedEdge(graph, 0, 11);
+  addUnweightedUndirectedEdge(graph, 7, 11);
+  addUnweightedUndirectedEdge(graph, 7, 6);
+  addUnweightedUndirectedEdge(graph, 7, 3);
+  addUnweightedUndirectedEdge(graph, 6, 5);
+  addUnweightedUndirectedEdge(graph, 3, 4);
+  addUnweightedUndirectedEdge(graph, 2, 3);
+  addUnweightedUndirectedEdge(graph, 2, 12);
+  addUnweightedUndirectedEdge(graph, 12, 8);
+  addUnweightedUndirectedEdge(graph, 8, 1);
+  addUnweightedUndirectedEdge(graph, 1, 10);
+  addUnweightedUndirectedEdge(graph, 10, 9);
+  addUnweightedUndirectedEdge(graph, 9, 8);
   console.log(graph);
 };
