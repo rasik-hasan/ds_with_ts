11304f29efa7b7c86fdccb63c4e8fd7ccbfaebd3
Rasik Hasan <rasik.hasan14@gmail.com>
2022-06-03 00:38:54 +0600

    delete and earn efficient

--- src/ProblemSolving/LC-740_delete_and_earn.ts
+++ src/ProblemSolving/LC-740_delete_and_earn.ts
@@ -3,9 +3,10 @@ export const testDeleteAndEarn = () => {
 
   const input1 = [3, 4, 2]; //6
   const input2 = [2, 2, 3, 3, 3, 4]; //9
+  const input3 = [3, 5, 7]; // 15
 
   function deleteAndEarn_v1(nums: number[]): number {
-    //O(n^3)
+    //O(2^n)
     if (nums.length === 0) {
       return 0;
     }
@@ -31,5 +32,44 @@ export const testDeleteAndEarn = () => {
     return Math.max(...results);
   }
 
-  console.log(deleteAndEarn_v1(input2));
+  //console.log(deleteAndEarn_v1(input2));
+
+  function deleteAndEarn_v2(nums: number[]): number {
+    const tally: { [key: string]: number } = {};
+
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] in tally) {
+        tally[nums[i]]++;
+      } else {
+        tally[nums[i]] = 1;
+      }
+    }
+
+    //console.log(tally);
+
+    const uniqueNums = Object.keys(tally).map((key) => parseInt(key));
+    //console.log(uniqueNums);
+
+    const results = [];
+    results[0] = tally[uniqueNums[0]] * uniqueNums[0];
+
+    //console.log(results[0]);
+
+    for (let i = 1; i < uniqueNums.length; i++) {
+      //console.log("uniqueNUms:", uniqueNums[i], uniqueNums[i - 1]);
+      let temp;
+
+      if (uniqueNums[i - 1] === uniqueNums[i] - 1) {
+        temp = uniqueNums[i] * tally[uniqueNums[i]] + (results[i - 2] ?? 0);
+      } else {
+        temp = uniqueNums[i] * tally[uniqueNums[i]] + results[i - 1];
+      }
+
+      results[i] = Math.max(results[i - 1], temp);
+    }
+
+    return results[results.length - 1];
+  }
+
+  console.log(deleteAndEarn_v2(input3));
 };
