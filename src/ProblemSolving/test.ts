a3a7d37b0916a8e736a1af6279fa0c0caf689fdc
rasik-hasan <rasik.hasan14@gmail.com>
2025-03-05 14:33:57 +0900

    test

--- src/ProblemSolving/test.ts
+++ src/ProblemSolving/test.ts
@@ -1,7 +1,36 @@
 export const test_test = () => {
   console.log("ready");
 
-  const test1 = "abacaba";
+  function longest_common_prefix(words: string[]): string {
+    if (words.length === 0) {
+      return "";
+    }
 
-  console.log(getString(test1));
+    if (words.length === 1) {
+      return words[0];
+    }
+
+    let firstString = words[0];
+
+    for (let i = 0; i < firstString.length; i++) {
+      for (let j = 1; j < words.length; j++) {
+        if (firstString[i] !== words[j][i]) {
+          return firstString.slice(0, i);
+        }
+      }
+    }
+
+    return firstString;
+  }
+
+  console.log(longest_common_prefix(["cat", "catalog", "catch"])); // "cat"
+  console.log(longest_common_prefix(["catalog", "catch", "category", "cat"])); // "cat"
+  console.log(
+    longest_common_prefix(["plain", "plan", "plant", "plastic", "planet"])
+  ); // "pla"
+  console.log(longest_common_prefix(["blue", "green", "yellow", "red"])); // ""
+  console.log(longest_common_prefix(["the first", "the second", "the third"])); // "the "
+  console.log(longest_common_prefix(["one word"])); // "one word"
+  console.log(longest_common_prefix([""])); // return ""
+  console.log(longest_common_prefix([])); // return ""
 };
